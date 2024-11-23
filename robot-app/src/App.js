602e1b5204241fa6340ac2fe7368af2ee88ecb06
Lynn Hakim <hk9794@wayne.edu>
2024-11-23 02:33:01 -0500

    Make app reactive

--- robot-app/src/App.js
+++ robot-app/src/App.js
@@ -1,32 +1,46 @@
 import React from "react";
-import { Container, Box, Typography } from "@mui/material";
+import { Container, Typography, Box } from "@mui/material";
 import TurtleControls from "./TurtleControls";
 import TurtleTopics from "./TurtleTopics";
 
 function App() {
   return (
     <Container>
-      <Box sx={{ textAlign: "center", marginTop: 4 }}>
-        <Typography variant="h3">
+      <Box 
+        sx={{
+          textAlign: "center", 
+          marginTop: 4, 
+          display: "flex", 
+          flexDirection: "column", 
+          alignItems: "center"
+        }}
+      >
+        <Typography variant="h3" sx={{ fontSize: { xs: '2rem', sm: '3rem', md: '4rem' } }}>
           Groot Web
         </Typography>
+
+       
         <Box
           component="canvas"
           id="turtleCanvas"
           sx={{
-            marginTop: 2,
             border: "1px solid black",
-            width: '500px', 
-            height: '500px',
-            mx: "auto", 
+            width: { xs: '100%', sm: '500px' },
+            height: { xs: '300px', sm: '400px', md: '500px' },
+            marginTop: 2,
           }}
         ></Box>
-        
-        <Box sx={{ marginTop: 4 }}>
-          <TurtleControls />
-        </Box>
 
-        <Box sx={{ marginTop: 4 }}>
+        <Box 
+          sx={{
+            marginTop: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: '100%'
+          }}
+        >
+          <TurtleControls />
           <TurtleTopics />
         </Box>
       </Box>
